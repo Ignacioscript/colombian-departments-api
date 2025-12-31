b5ffbae42a6f408a0d954f3d5c32a6338ef2c466
Ignacio Script <ignacio.script@gmail.com>
2025-12-31 18:43:54 -0500

    feat(database) create seeders and factories for department and city models

---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $departments = include database_path('seeders/data/departments_list.php');
+
+
+        DB::table('departments')->insert($departments);
+    }
+}
